fbe0d410822e2d0375ca17d812a3a96c69fb32f7
asatara <andrej.satara@gmail.com>
2015-02-03 14:05:03 -0800

    Moving and renaming cmdLine to exec in shared folder

--- shared/exec/exec.go
+++ shared/exec/exec.go
@@ -1,7 +1,6 @@
-package cmdLine
+package exec
 
 import "fmt"
-import "os"
 import "sync"
 import "strings"
 import "os/exec"
@@ -11,28 +10,28 @@ var waitGroup sync.WaitGroup
 func runCommand(command string) string {
 	//var waitGroup sync.WaitGroup
 	waitGroup.Add(1)
-	exe_cmd(command, &waitGroup)
+	return execute(command, &waitGroup)
 }
 
 func getDiskSpace() string {
 	waitGroup.Add(1)
-	return exe_cmd("df", &waitGroup)
+	return execute("df", &waitGroup)
 }
 
 func uptime() string {
 	waitGroup.Add(1)
-	return exe_cmd("uptime", &waitGroup)
+	return execute("uptime", &waitGroup)
 }
 
 func currentLoad() string {
 	waitGroup.Add(1)
-	var date = exe_cmd("uptime", &waitGroup)
+	var date = execute("uptime", &waitGroup)
 	parts := strings.Fields(date)
 	parts = parts[(len(parts) - 3):len(parts)]
 	return string(parts[0] + " " + parts[1] + " " + parts[2])
 }
 
-func exe_cmd(cmd string, wg *sync.WaitGroup) string {
+func execute(cmd string, wg *sync.WaitGroup) string {
 	//function from http://stackoverflow.com/questions/20437336/how-to-execute-system-command-in-golang-with-unknown-arguments
 	//	fmt.Println("command is ", cmd)
 	// splitting head => g++ parts => rest of the command
